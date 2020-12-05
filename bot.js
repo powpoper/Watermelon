17c59dfdae4d25f9bf53660fcc0c8ac1d6afc523
powpoper <andrew.manashi@gmail.com>
2020-12-05 01:57:34 -0600

    refactored bot.js

--- bot.js
+++ bot.js
@@ -12,29 +12,33 @@ client.on('message', msg => {
     checkIfWatermelonRoleExists(msg);
     reactWithWatermelon(msg);
     botReply(msg);
-
-
+    helpWatermelonFarmer(msg);
 });
 
-
+const helpWatermelonFarmer = (msg) => {
+    if(msg.content === '!help-watermelon-farmer'){
+        let role = msg.guild.roles.cache.find(x => x.name === USER_ROLE);
+        msg.member.roles.add(role);
+        msg.reply("Thanks for helping me out! Now whenever you type watermelon in a message I'll share my harvest with you. 🍉")
+    }
+};
 
 const reactWithWatermelon = (msg) => {
-    if (msg.content.toLowerCase().indexOf("watermelon") > -1) {
+    const isWatermelonRole = msg.member.roles.cache.map(x => x.name).indexOf(USER_ROLE) > -1;
+
+    if (isWatermelonRole && msg.content.toLowerCase().indexOf("watermelon") > -1) {
         msg.react('🍉');
       }
 };
 const botReply = (msg) => {
     if(msg.mentions.members.map(x => x.user.username).indexOf("Watermelon Farmer Bot") > -1) {
-        msg.reply("looks like you asked for me. If you need me just type watermelon in a message.")
+        msg.reply("looks like you asked for me, but I'm a little busy right now. I can use your help though! Message '!help-watermelon-farmer' to help me harvest watermelons.")
     }
 };
 
 const checkIfWatermelonRoleExists = (msg) => {
-    let role = msg.guild.roles.cache.find(x => x.name === USER_ROLE);
-    console.log(role)
-    console.log(typeof role)
+    const role = msg.guild.roles.cache.find(x => x.name === USER_ROLE);
     if (role === undefined) {
-        console.log(true)
         msg.guild.roles.create({
             data:{
             name:"Watermelon",
